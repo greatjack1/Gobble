6d84e2998cd8792390fcd0f0f929cff8c187d462
greatjack1 <yaakovrim@gmail.com>
2017-12-03 15:23:21 -0500

    partially implmented amazon tests

---
+++ gobbleTests/KeyStoreTests/AmazonApiTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace gobbleTests.KeyStoreTests
+{
+    public class AmazonApiTests
+    {
+       
+            [Fact]
+            public void FullTest(){
+                
+            }
+
+    }
+}
